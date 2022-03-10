40b56b402afd145fe7d22df651b924f93f1e829b
sp381 <sarah.powell381@gmail.com>
2022-03-10 12:09:27 -0700

    added some code for the Youtube API in JS

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,10 @@
 var APIkey = "AIzaSyA2ChTeollJqbj7hUF54Y88r9RysfXB2xc";
-GET https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&id=nWr4r-3bHBc&key=AIzaSyA2ChTeollJqbj7hUF54Y88r9RysfXB2xc
+var requestUrl = 'https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&id=nWr4r-3bHBc&key=AIzaSyA2ChTeollJqbj7hUF54Y88r9RysfXB2xc';
+var youtubeContainerEl = document.querySelector("#youtube-container");
+var musicVideo = document.querySelector("#music-video");
 
-fetch("https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&id=nWr4r-3bHBc&key=AIzaSyA2ChTeollJqbj7hUF54Y88r9RysfXB2xc", {
-	"method": "GET",
+fetch("requestUrl", {
+	"method": "get",
 	"headers": {
 		"x-rapidapi-host": "ytstream-download-youtube-videos.p.rapidapi.com",
 		"x-rapidapi-key": "980d637cdbmsh81ca7e280815ecfp1293afjsna174cb76791a"
@@ -14,6 +16,6 @@ fetch("https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDe
 .catch(err => {
 	console.error(err);
 });
-Authorization: Bearer [YOUR_ACCESS_TOKEN]
-Accept: application/json
+//Authorization: Bearer [YOUR_ACCESS_TOKEN]
+//Accept: application/json
 
