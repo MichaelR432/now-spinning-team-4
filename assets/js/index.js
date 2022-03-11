16b1418f29a9e064bd01054b4be33dfb5c4aa46b
Paquito81 <95262020+Paquito81@users.noreply.github.com>
2022-03-10 23:31:42 -0700

    added background image

---
+++ assets/js/index.js
@@ -0,0 +1,10 @@
+module.exports = {
+    theme: {
+        extend: {
+            backgroundImage: {
+                'hero-pattern': "url('assets/images/pexels-francesco-paggiaro-2209315.jpg')",
+                
+            }
+        }
+    }
+}
