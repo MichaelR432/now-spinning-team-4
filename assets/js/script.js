ad4e14898654777e8525dc08499c35e6bdb0ccac
sp381 <sarah.powell381@gmail.com>
2022-03-11 14:31:26 -0700

    the music video shows up in the console.log. Next display on the webpage

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,24 +1,27 @@
 var APIkey = "AIzaSyA2ChTeollJqbj7hUF54Y88r9RysfXB2xc";
-var requestUrl = "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&id=nWr4r-3bHBc&key=AIzaSyA2ChTeollJqbj7hUF54Y88r9RysfXB2xc";
+const requestUrl = "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&id=nWr4r-3bHBc&key=AIzaSyA2ChTeollJqbj7hUF54Y88r9RysfXB2xc";
 var youtubeContainerEl = document.querySelector("#youtube-container");
-var musicVideo = document.querySelector("#music-video");
+const musicVideo = document.querySelector("#music-video");
 
-fetch(requestUrl, {
-}).then(response => response.json())
+fetch(requestUrl)
+.then(response => response.json())
 	.then((result) => {
 		console.log(result);
+		if(!response.ok) {
+			
+		}
+		return response.musicVideo
 	})
 
 .catch(err => {
 	console.error(err);
 });
 
-var displayVideo = function() {
-		document.getElementById("#music-video").innerHTML
-	return;
-}
+//var displayVideo = function() {
+//		document.getElementById("#music-video")
+//	return;
+//}
  //look at append child 
  // var to display video
  // add = to line 17 with variable to display video
  // probably need to do a createElementById for line 17
- 
